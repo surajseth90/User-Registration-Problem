8b79d50104b16bbba8784e2c654d4f1b87ded424
surajseth90 <surajseth90@gmail.com>
2021-04-26 19:41:07 +0530

    Refactor the code

--- src/Email.java
+++ src/Email.java
@@ -11,10 +11,7 @@ public class Email {
 		if(CheckCondition.checkCondition(email,emailCondition))
 			System.out.println();
 		else {
-			System.out.println("Invalid Input : Please Enter in a proper format "+"\n"
-					+ "( 1. Name must have only charater values," + "\n"
-					+ "  2. Name must have minimum 3 letters," + "\n"
-					+ "  3. First letter must be Capital ) "+"\n");
+			System.out.println("Invalid Input : Please Enter in a proper format : eg. abc.xyz@bl.co.in");
 			Email.email();
 			}
 		return emailCondition;
