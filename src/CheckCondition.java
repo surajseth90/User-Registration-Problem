70f0b75443e3773950d6accabe9c6d6aba144e9a
surajseth90 <surajseth90@gmail.com>
2021-04-26 19:07:57 +0530

    code for use case 2 is added successfully

--- src/CheckCondition.java
+++ src/CheckCondition.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,16 +11,39 @@ public class CheckCondition {
 		return result;
 	}
 	
-	public static String firstName(String firstName) {
+	public static String firstName() {
+		Scanner scanner = new Scanner(System.in);
 		String firstNameCondition = "[A-Z][a-z]{3,}";
+		System.out.println("Enter your first name : ");
+		String firstName = scanner.nextLine();
 		CheckCondition.checkCondition(firstName,firstNameCondition);
 		if(CheckCondition.checkCondition(firstName,firstNameCondition))
-			System.out.println(firstName);
-		else
+			System.out.println();
+		else {
 			System.out.println("Invalid Input : Please Enter in a proper format "+"\n"
 					+ "( 1. Name must have only charater values," + "\n"
 					+ "  2. Name must have minimum 3 letters," + "\n"
-					+ "  3. First letter must be Capital ) ");
+					+ "  3. First letter must be Capital ) "+"\n");
+			CheckCondition.firstName();
+			}
 		return firstNameCondition;
+	}
+	
+	public static String lastName() {
+		Scanner scanner = new Scanner(System.in);
+		String lastNameCondition = "[A-Z][a-z]{3,}";
+		System.out.println("Enter your last name : ");
+		String lastName = scanner.nextLine();
+		CheckCondition.checkCondition(lastName,lastNameCondition);
+		if(CheckCondition.checkCondition(lastName,lastNameCondition))
+			System.out.println();
+		else {
+			System.out.println("Invalid Input : Please Enter in a proper format "+"\n"
+					+ "( 1. Name must have only charater values," + "\n"
+					+ "  2. Name must have minimum 3 letters," + "\n"
+					+ "  3. First letter must be Capital ) " +"\n");
+			CheckCondition.lastName();
+		}
+		return lastNameCondition;
 	}
 }
--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class UserRegistration {
 
 	public static void main(String[] args) {	
-		Scanner scanner = new Scanner(System.in);
-		String firstName = scanner.nextLine();
-		CheckCondition.firstName(firstName);	
+			CheckCondition.firstName();
+			CheckCondition.lastName();
 	}
-}
+	
+	}
+	
